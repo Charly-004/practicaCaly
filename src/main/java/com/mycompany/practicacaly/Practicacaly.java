ec937cc7dd67c6bb88c2a62bb18270782b74e3fd
Charly-004 <charlycortes004@gmail.com>
2024-11-01 22:41:57 +0100

    tercera practica

---
+++ src/main/java/com/mycompany/practicacaly/Practicacaly.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practicacaly;
+
+/**
+ *
+ * @author charly
+ */
+public class Practicacaly {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
